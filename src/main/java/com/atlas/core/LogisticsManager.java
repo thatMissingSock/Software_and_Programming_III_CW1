5c5a8840abace5e8bc337388c46e2bdd201eb1f1
thatMissingSock <mahmed96@student.bbk.ac.uk>
2026-02-08 18:29:03 +0000

    added a method in which the unit test can upon and view a 'master' shipment list. I still can't run ANY unit tests but the new methods have some uses so I'm inching closer :))

--- src/main/java/com/atlas/core/LogisticsManager.java
+++ src/main/java/com/atlas/core/LogisticsManager.java
@@ -1,5 +1,6 @@
 package com.atlas.core;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.atlas.models.Shipment;
 import com.atlas.strategies.StrategySwitch;
@@ -8,16 +9,27 @@ import com.atlas.strategies.StrategySwitch;
 
 public class LogisticsManager {
 
+/*
+BELOW is a chunk of code I THINK is needed to satisfy the test code so it can actually run. All it is, is initialising a
+masterList of shipments and its 'abilities'
+ */
+    // I need to add an 'addShipment' so the tests that use those can run
+    private final List<Shipment> masterList = new ArrayList<>(); // initialising it
+
+    public void addShipment(Shipment shipment) { // creating a method to add shipments to the masterList
+        masterList.add(shipment);
+    }
+
+    public List<Shipment> getMasterList() { // creating a method to call the masterList
+        return masterList;
+    }
+
     // we need to create a new object testing for null and other things
     public double calculateSingleCost(Shipment s) { // changed the name to match the test
         if (s == null) throw new NullPointerException("Shipment cannot be null, does not make sense"); // null test
         return StrategySwitch.fromType(s.getType()).calculateCost(s); // use the calculateCost after returning the types
     }
 
-    //
-
-
-    private List<Shipment> masterShipmentList;
 
     /**
      * MLO8: Use of Streams to filter and aggregate data.
